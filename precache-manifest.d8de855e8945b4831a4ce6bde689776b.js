f09a7032632ab8d9cda8ce85c9e43702e5a1f5e0
viocuong <viocuong@users.noreply.github.com>
2020-03-12 17:29:23 +0700

    Updates

---
+++ precache-manifest.d8de855e8945b4831a4ce6bde689776b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1d8d7617603bdbe6790f186d1af78c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "48fdd1ea17246c7df0ff",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "87e7221daf34f4c82e20",
+    "url": "/my-app/static/js/2.10c52673.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-app/static/js/2.10c52673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48fdd1ea17246c7df0ff",
+    "url": "/my-app/static/js/main.332d3522.chunk.js"
+  },
+  {
+    "revision": "d4e1f3dc1f7ec52f3603",
+    "url": "/my-app/static/js/runtime-main.5f230656.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
